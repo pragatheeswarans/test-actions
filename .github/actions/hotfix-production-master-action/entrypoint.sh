46fbf0e8a399cfee5e6a2f5121e7e6a56ea3a09e
pragatheeswaranshf <pragatheeswaran.s@happyfox.com>
2020-05-17 16:13:32 +0530

    test

--- .github/actions/hotfix-production-master-action/entrypoint.sh
+++ .github/actions/hotfix-production-master-action/entrypoint.sh
@@ -4,7 +4,6 @@ echo "The merge commit is $1"
 echo "The github event path is $GITHUB_EVENT_PATH"
 echo "::set-output name=commit-hash::$1"
 
-head=$(jq -r .head.ref ${GITHUB_EVENT_PATH})
-echo "Head is $head"
-git checkout -b hotfix_backport origin/${head}
+git fetch origin
+git checkout -b hotfix_backport origin/master
 git push origin hotfix_backport
